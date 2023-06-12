8a57954573dd33646dd75c83bdf364fe5c6f2dd9
unknown <gayathrisureshkumar1222@gmail.com>
2023-06-12 16:56:59 +0530

    to take sometine

--- src/org/test/One.java
+++ src/org/test/One.java
@@ -3,5 +3,10 @@ package org.test;
 public class One {
 public static void main(String[] args) {
 	System.out.println("Gayathri is a Beautiful Girl");
+	
+}
+
+private void tk() {
+System.out.println("Am going to canada");
 }
 }
